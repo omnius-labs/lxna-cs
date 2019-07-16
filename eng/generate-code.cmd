9c7159fff53c02db9fac29f45bbf89b591b49d56
Lyrise <lyrise1984@gmail.com>
2019-07-17 05:36:19 +0900

    Add 設定ファイル類をOmnixプロジェクトから移植

--- eng/generate-code.cmd
+++ eng/generate-code.cmd
@@ -2,16 +2,11 @@ setlocal
 
 setx DOTNET_CLI_TELEMETRY_OPTOUT 1
 
-if %PROCESSOR_ARCHITECTURE% == x86 (
-    set TOOL_PATH=%cd%\tools\win-x86\Omnix.Serialization.RocketPack.CodeGenerator\Omnix.Serialization.RocketPack.CodeGenerator.exe
-)
+set TOOL_PATH=%cd%\tools\win\Omnix.Serialization.RocketPack.CodeGenerator\Omnix.Serialization.RocketPack.CodeGenerator.exe
 
-if %PROCESSOR_ARCHITECTURE% == AMD64 (
-    set TOOL_PATH=%cd%\tools\win-x64\Omnix.Serialization.RocketPack.CodeGenerator\Omnix.Serialization.RocketPack.CodeGenerator.exe
-)
+"%TOOL_PATH%" %cd%\formats\Xeus.Messages.rpf %cd%\src\Xeus.Messages\_RocketPack\Messages.generated.cs
 
-"%TOOL_PATH%" %cd%\formats\Lxna.Core.Contents.rpf %cd%\src\Lxna.Core\Contents\_RocketPack\Messages.generated.cs
-
-"%TOOL_PATH%" %cd%\formats\Lxna.Messages.rpf %cd%\src\Lxna.Messages\_RocketPack\Messages.generated.cs
-
-"%TOOL_PATH%" %cd%\formats\Lxna.Rpc.rpf %cd%\src\Lxna.Rpc\_RocketPack\Messages.generated.cs
+"%TOOL_PATH%" %cd%\formats\Xeus.Core.Connections.Internal.rpf %cd%\src\Xeus.Core\Connections\Internal\_RocketPack\Messages.generated.cs
+"%TOOL_PATH%" %cd%\formats\Xeus.Core.Internal.rpf %cd%\src\Xeus.Core\Internal\_RocketPack\Messages.generated.cs
+"%TOOL_PATH%" %cd%\formats\Xeus.Core.Contents.Internal.rpf %cd%\src\Xeus.Core\Contents\Internal\_RocketPack\Messages.generated.cs
+"%TOOL_PATH%" %cd%\formats\Xeus.Core.Exchange.Internal.rpf %cd%\src\Xeus.Core\Exchange\Internal\_RocketPack\Messages.generated.cs
