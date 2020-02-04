3dbecd6e227d95771c6e4c0f31059684d7d254ce
Lyrise <lyrise1984@gmail.com>
2020-02-04 23:31:08 +0900

    Fix プロジェクト構成の調整

---
+++ eng/gen-code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+BIN_DIR=$PWD/bin/tools/linux
+TOOL_PATH=$BIN_DIR/Omnius.Core.Serialization.RocketPack.DefinitionCompiler/Omnius.Core.Serialization.RocketPack.DefinitionCompiler
+INCLUDE="-i $PWD/fmt $PWD/refs/core/fmt"
+
+"$TOOL_PATH" $PWD/fmt/Omnius.Lxna.Service/Omnius.Lxna.Service.rpd $INCLUDE -o $PWD/src/Omnius.Lxna.Service/_RocketPack/Messages.generated.cs
+"$TOOL_PATH" $PWD/fmt/Omnius.Lxna.Service.Implements/Omnius.Lxna.Service.Internal.rpd $INCLUDE -o $PWD/src/Omnius.Lxna.Service.Implements/Internal/_RocketPack/Messages.generated.cs
--- eng/generate-code.cmd
+++
@@ -1,9 +0,0 @@
-setlocal
-
-setx DOTNET_CLI_TELEMETRY_OPTOUT 1
-
-set TOOL_PATH=%cd%\tools\win\Omnix.Serialization.RocketPack.CodeGenerator\Omnix.Serialization.RocketPack.CodeGenerator.exe
-
-"%TOOL_PATH%" %cd%\formats\Lxna.Core.Internal.rpf %cd%\src\Lxna.Core\Internal\_RocketPack\Messages.generated.cs
-"%TOOL_PATH%" %cd%\formats\Lxna.Messages.rpf %cd%\src\Lxna.Messages\_RocketPack\Messages.generated.cs
-"%TOOL_PATH%" %cd%\formats\Lxna.Rpc.rpf %cd%\src\Lxna.Rpc\_RocketPack\Messages.generated.cs
