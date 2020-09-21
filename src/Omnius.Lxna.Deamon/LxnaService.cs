16663d4db88e3326c73ebd7c460ae2ce77ce1676
Lyrise <lyrise1984@gmail.com>
2020-09-21 14:27:44 +0900

    fix

---
+++ src/Omnius.Lxna.Deamon/LxnaService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Omnius.Lxna.Rpc;
+using Omnius.Lxna.Components.Connectors;
+using Omnius.Lxna.Components.Engines;
+using Omnius.Lxna.Components.Models;
+using Omnius.Lxna.Deamon.Models;
+using System.IO;
+using Omnius.Core;
+using Omnius.Core.Network.Upnp;
+using Omnius.Core.Network.Proxies;
+using Omnius.Lxna.Deamon.Internal;
+
+namespace Omnius.Lxna.Rpc{
+class LxnaService: ILxnaService{
+
+}
+}
---
+++ src/Omnius.Lxna.Ui.Desktop/ViewModels/SearchControlViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Omnius.Lxna.Ui.Desktop.ViewModels
+{
+    public class SearchControlViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
