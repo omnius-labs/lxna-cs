2800804e32ebcdfe7970d25a8f919d9541b15991
Lyrise <lyrise1984@gmail.com>
2024-03-03 14:14:58 +0900

    fix

--- build-linux.sh
+++ build-linux.sh
@@ -6,4 +6,4 @@ rm -rf ./pub/linux-x64/*
 export NativeDepsPlatform=linux
 export PlatformTarget=x64
 
-dotnet publish ./src/Omnius.Lxna.Ui.Desktop -p:PublishSingleFile=true --runtime linux-x64 --configuration Release --self-contained true --output ./pub/linux-x64/bin/ui-desktop
+dotnet publish ./src/Omnius.Lxna.Ui.Desktop -p:PublishSingleFile=true --runtime linux-x64 --configuration Release --self-contained true --output ./pub/linux-x64/
