06ce1c95d79cbd638f233178676f40385d86ee5c
Lyrise <lyrise1984@gmail.com>
2024-05-19 18:02:41 +0900

    fix

--- src/Lxna.Launcher/Runner.cs
+++ src/Lxna.Launcher/Runner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Globalization;
 using System.Net;
@@ -42,10 +43,10 @@ public static class Runner
                     UseShellExecute = false,
                 };
 
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                var envPath = Path.Combine(basePath, ".env." + GetCurrentPlatform());
+                foreach (var (key, value) in ParseEnvFile(envPath))
                 {
-                    uiDesktopProcessInfo.Environment["LANG"] = "en_US.UTF-8";
-                    uiDesktopProcessInfo.Environment["AVALONIA_SCREEN_SCALE_FACTORS"] = "XWAYLAND0=2";
+                    uiDesktopProcessInfo.Environment[key] = value;
                 }
 
                 using var uiDesktopProcess = Process.Start(uiDesktopProcessInfo);
@@ -58,5 +59,41 @@ public static class Runner
         {
             _logger.Error(e, "Unexpected Exception");
         }
+    }
+
+    public static string GetCurrentPlatform()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return "win";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            return "linux";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            return "osx";
+        }
+        else
+        {
+            throw new PlatformNotSupportedException();
+        }
+    }
+
+    private static IReadOnlyDictionary<string, string> ParseEnvFile(string path)
+    {
+        using var reader = new StreamReader(path);
+        var builder = ImmutableDictionary.CreateBuilder<string, string>();
+
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            var parts = line.Split('=', 2);
+            if (parts.Length != 2) continue;
+            builder[parts[0]] = parts[1];
+        }
+
+        return builder.ToImmutable();
     }
 }
