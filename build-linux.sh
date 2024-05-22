738ab810799ac5572e9ad6977aafc9f279cca104
Lyrise <lyrise1984@gmail.com>
2024-05-22 20:30:40 +0900

    fix

--- build-linux.sh
+++ build-linux.sh
@@ -6,5 +6,5 @@ rm -rf ./pub/linux-x64/*
 export NativeDepsPlatform=linux
 export PlatformTarget=x64
 
-dotnet publish ./src/Lxna.Ui.Desktop -p:PublishSingleFile=true --runtime linux-x64 --configuration Release --self-contained true --output ./pub/linux-x64/
-dotnet publish ./src/Lxna.Updater -p:PublishSingleFile=true --runtime linux-x64 --configuration Release --self-contained true --output ./pub/linux-x64/updater/
+dotnet publish ./src/Lxna.Ui.Desktop -p:PublishSingleFile=true --runtime linux-x64 --configuration Release --self-contained true --output ./pub/linux-x64/bin/
+dotnet publish ./src/Lxna.Updater -p:PublishSingleFile=true --runtime linux-x64 --configuration Release --self-contained true --output ./pub/linux-x64/bin/updater/
